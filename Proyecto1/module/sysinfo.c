c64edffa0539cf2dd7d47443aa4098adc1007d30
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-08-26 00:41:02 -0600

    Refactor sysinfo module to optimize CPU usage calculation

--- Proyecto1/module/sysinfo.c
+++ Proyecto1/module/sysinfo.c
@@ -77,28 +77,15 @@ static void get_memory_info(struct seq_file *m){
 }
 
 // Función para verificar si un proceso pertenece a un contenedor Docker
-static int is_docker_container(struct task_struct *task) {
-    // Verifica si el proceso padre es `containerd-shim`
-    if (task && strstr(task->comm, "containerd-shim") != NULL) {
-        return 1;
-    }
+// static int is_docker_container(struct task_struct *task) {
+//     // Verifica si el proceso padre es `containerd-shim`
+//     if (task && strstr(task->comm, "containerd-shim") != NULL) {
+//         return 1;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
-static void calculate_cpu_time(struct task_struct *task, unsigned long *total_time) {
-    struct task_struct *child;
-    struct list_head *list;
-
-    // Sumar el tiempo de CPU del proceso actual
-    *total_time += task->utime + task->stime;
-
-    // Recorrer los procesos hijos y sumar sus tiempos de CPU
-    list_for_each(list, &task->children) {
-        child = list_entry(list, struct task_struct, sibling);
-        calculate_cpu_time(child, total_time);
-    }
-}
 
 static void get_container_processes_info(struct seq_file *m) {
     struct task_struct *task;
@@ -107,62 +94,90 @@ static void get_container_processes_info(struct seq_file *m) {
     struct sysinfo i;
     si_meminfo(&i);
     signed long toal_ram = i.totalram * i.mem_unit;
-    unsigned long total_time;
-    unsigned long process_lifetime;
-    unsigned long cpu_usage;
-    // unsigned long total_cpu_time = jiffies_to_msecs(get_jiffies_64());
 
+    unsigned long utime, stime, start_time,uptime;
+    unsigned long uptime_sec, process_elapsed_sec,process_ussage_sec;
+    unsigned long process_usage;
+    struct timespec64 uptime_ts;
+
+    // unsigned long total_jiffies = jiffies;
     for_each_process(task) {
-        if (is_docker_container(task)) {
+        if (task->pid == 3355) {
+            // struct sched_entity *se = &task->se;
             struct mm_struct *mm = task->mm;
             unsigned long rss = 0, vsz = 0;
+            unsigned long rssKB = 0, vszKB = 0;
             unsigned long porc_ram = 0;
-           
-           
-            // unsigned long cpu_percentage = (total_time * 100) / (total_cpu_time * num_online_cpus());
-
             if (mm) {
                 rss = get_mm_rss(mm) << PAGE_SHIFT;
+                rssKB = rss / 1024;
                 vsz = mm->total_vm << PAGE_SHIFT;
+                vszKB = vsz / 1024;
             }
             unsigned long total_ram_pages;
             total_ram_pages = totalram_pages();
             if(found){
                 seq_printf(m, ",\n");
             }
+            
+            start_time = task->start_time;
+            // utime = utime / 1000;
+            // stime = stime / 1000;
+            // start_time = start_time / 1000;
+            // uptime_sec = task->real_start_time;
+            uptime_sec = ktime_get_boottime_seconds();
+            // unsigned long long exec_runtime = se->sum_exec_runtime;
+            // unsigned long long total_jiffies = jiffies;
+            // unsigned long total_time = task->utime + task->stime;
+            task_lock(task);
+            utime = task->utime;
+            stime = task->stime;
+            task_unlock(task);
+            // utime /= (HZ*10);
+            // stime /= (HZ*10);
+            start_time /= (HZ*10000);
+            start_time /= 100;
+            // utime /= 100;
+            // stime /= 100;
+            // process_elapsed_sec = start_time-jiffies_to_clock_t(task->start_time);
+            process_elapsed_sec =  jiffies_to_msecs(jiffies - task->start_time) / 1000;
+            process_ussage_sec = jiffies_to_msecs(task->utime)/(HZ*10) + jiffies_to_msecs(task->stime)/(HZ*10);
+            // process_ussage_sec /= (HZ*10);
+            // process_ussage_sec /= 100;
+            ktime_get_boottime_ts64(&uptime_ts);
+            uptime = uptime_ts.tv_sec;
+            // process_elapsed_sec /= (HZ*10);
+            // process_elapsed_sec /= 100;
 
-            if(task->children.next != NULL){
-                struct task_struct *child;
-                list_for_each_entry(child, &task->children, sibling){
-                    struct mm_struct *mm_child = child->mm;
-                    if(mm_child){
-                        rss += get_mm_rss(mm_child) << PAGE_SHIFT;
-                        vsz += mm_child->total_vm << PAGE_SHIFT;
-                    }
-                }
-                // cpu_percentage = (total_time * 100) / (total_cpu_time * num_online_cpus());
+            if(process_elapsed_sec > 0){
+                // process_usage = (process_ussage_sec) / (process_elapsed_sec);
+                process_usage = (process_ussage_sec*HZ*100) / (jiffies-(task->start_time/(HZ*10000))*num_online_cpus());
             }
-            total_time = 0;
-            calculate_cpu_time(task, &total_time);
-
-            process_lifetime = jiffies - task->start_time;
-
-            // Calcular el porcentaje de uso de CPU
-            if (process_lifetime > 0) {
-                cpu_usage = (total_time * 100) / (process_lifetime * HZ / 100);
-            } else {
-                cpu_usage = 0;
-            }
-
+            // process_usage = (total_time * 10000) / (total_jiffies * num_online_cpus());
+            // process_usage = jiffies_to_msecs(process_usage);
+            // if(task->children.next != NULL){
+            //     struct task_struct *child;
+            //     list_for_each_entry(child, &task->children, sibling){
+            //         struct mm_struct *mm_child = child->mm;
+            //         if(mm_child){
+            //             rss += get_mm_rss(mm_child) << PAGE_SHIFT;
+            //             vsz += mm_child->total_vm << PAGE_SHIFT;
+            //         }
+            //     }
+            //     // cpu_percentage = (total_time * 100) / (total_cpu_time * num_online_cpus());
+            // }
+            
+            
             seq_printf(m, "{\n");
             seq_printf(m, "\"pid\": %d,\n", task->pid);
             seq_printf(m, "\"name\": \"%s\",\n", get_process_cmdline(task));
             seq_printf(m, "\"cmdline\": \"%s\",\n", task->comm);
-            seq_printf(m, "\"vsz\": %lu,\n", vsz);
-            seq_printf(m, "\"rss\": %lu,\n", rss);
+            seq_printf(m, "\"vsz\": %lu,\n", vszKB);
+            seq_printf(m, "\"rss\": %lu,\n", rssKB);
             porc_ram = (rss * 100) / toal_ram;
             seq_printf(m, "\"mem percent\": %lu,\n", porc_ram);
-            seq_printf(m, "\"cpu percent\": %lu\n", cpu_usage);
+            // seq_printf(m, "\"cpu percent\": %lu\n", process_usage);
+            seq_printf(m, "\"process_usage\": %lu\n", process_usage);
             seq_printf(m, "}");
             found = true;
         }
