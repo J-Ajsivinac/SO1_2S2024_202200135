04f531de46b0b7f07304f557aa37f67d11a788ce
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-09-02 22:33:37 -0600

    chore: Update .gitignore and add CPU and memory usage graphs

--- Proyecto1/python_server/main.py
+++ Proyecto1/python_server/main.py
@@ -6,13 +6,24 @@ import matplotlib.pyplot as plt
 from typing import List
 from fastapi import FastAPI # type: ignore
 from models.models import LogProcess
-app = FastAPI()
 
+def gen_image(data, type):
+    type_name = 'CPU' if type == 'cpu_usage' else 'Memoria'
+    df_heatmap = data.groupby(['timestamp', 'container_id'])[type].mean().unstack()
+    plt.style.use('dark_background')
+    sns.heatmap(df_heatmap, cmap=sns.cubehelix_palette(as_cmap=True), annot=True, fmt=".2f", linewidths=0.5)
+    plt.title(f'Heatmap de uso de {type_name}')
+    plt.xlabel('Container ID')
+    plt.ylabel('Tiempo')
+    plt.grid(True)
+    plt.savefig(f'{type}_graph.png', dpi=300)
+    plt.close()
+
+app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "test"}
-
 
 @app.post("/logs")
 def get_logs(logs_proc: List[LogProcess]):
@@ -31,9 +42,9 @@ def get_logs(logs_proc: List[LogProcess]):
     new_logs = [log.dict() for log in logs_proc]
     existing_logs.extend(new_logs)
 
-    # # Escribimos la lista de logs en el archivo logs.json
-    # with open(logs_file, 'w') as file:
-    #     json.dump(existing_logs, file, indent=4)
+    # Escribimos la lista de logs en el archivo logs.json
+    with open(logs_file, 'w') as file:
+        json.dump(existing_logs, file, indent=4)
 
     return {"received": existing_logs}
 
@@ -49,38 +60,10 @@ def get_graph():
 
     df = pd.DataFrame(existing_logs)
     df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.strftime('%Y-%m-%d %H:%M:%S')
-    df_heatmap = df.groupby(['timestamp', 'container_id'])['cpu_usage'].mean().unstack()
     
-    plt.style.use('dark_background')
-    sns.heatmap(df_heatmap, cmap=sns.cubehelix_palette(as_cmap=True), annot=True, fmt=".2f", linewidth=0.5)
-    plt.title('Heatmap de uso de CPU')
-    plt.xlabel('Container ID')
-    plt.ylabel('Tiempo')
-    plt.grid(True)
-    plt.savefig('cpu_usage_graph.png', dpi=300)
-    plt.close()
-
+    # Heatmap de uso de CPU
+    gen_image(df, 'cpu_usage')
     # Heatmap de uso de memoria
-    df_memory_heatmap = df.groupby(['timestamp', 'container_id'])['memory_usage'].mean().unstack()
-    plt.figure(figsize=(10, 6))
-    plt.style.use('dark_background')
-    sns.heatmap(df_memory_heatmap, cmap=sns.cubehelix_palette(as_cmap=True), annot=True, fmt=".2f", linewidths=0.5)
-    plt.title('Heatmap de uso de Memoria')
-    plt.xlabel('Container ID')
-    plt.ylabel('Tiempo')
-    plt.grid(True)
-    plt.savefig('memory_usage_graph.png', dpi=300)
-    plt.close() 
+    gen_image(df, 'memory_usage')
+
     return {"message": "Graph created"}
-
-
-def gen_image(data, type):
-    df_heatmap = data.groupby(['timestamp', 'container_id'])[type].mean().unstack()
-    plt.style.use('dark_background')
-    sns.heatmap(df_heatmap, cmap=sns.cubehelix_palette(as_cmap=True), annot=True, fmt=".2f", linewidths=0.5)
-    plt.title(f'Heatmap de uso de {type}')
-    plt.xlabel('Container ID')
-    plt.ylabel('Tiempo')
-    plt.grid(True)
-    plt.savefig(f'{type}_usage_graph.png', dpi=300)
-    plt.close()
