9c6b2197fbfcaf16d95121a7cc8048ca36f57621
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-10-09 22:44:28 -0600

    Refactor Kubernetes ingress configuration for Proyecto2
    - Renamed locust-dev.yaml to locust-deployment.yaml
    - Added namespace: so1-proyecto2 to the Ingress metadata
    - Updated paths for engineering and agronomy services

--- Proyecto2/test/locustfile.py
+++ Proyecto2/test/locustfile.py
@@ -6,12 +6,24 @@ class StudentTraffic(HttpUser):
 
     def on_start(self):
         # Cargar el archivo JSON al iniciar Locust
-        with open('estudiantes.json', 'r') as file:
+        with open('test.json', 'r') as file:
             self.students_data = json.load(file)
 
     @task
     def send_traffic(self):
         # Iterar sobre los datos cargados desde el archivo JSON
         for student in self.students_data:
-            response = self.client.post(f"/{student['faculty'].lower()}", json=student)
-            print(f"Enviando datos: {student} - Respuesta: {response.status_code}")
+            # response = self.client.post(f"/{student['faculty'].lower()}", json=student)
+            # print(f"Enviando datos: {student} - Respuesta: {response.status_code}")
+            if student['faculty'].lower() == 'ingenieria':
+                self.client.post("http://34.133.16.40/engineering", json=student)
+                print(f"Enviando datos: {student}")
+            elif student['faculty'].lower() == 'agronomia':
+                self.client.post("http://34.133.16.40/agronomy", json=student)
+                print(f"Enviando datos: {student}")
+
+
+# Ejecutar Locust
+# locust -f test/locustfile.py --host=http://localhost:8080
+# http://localhost:8089/
+
