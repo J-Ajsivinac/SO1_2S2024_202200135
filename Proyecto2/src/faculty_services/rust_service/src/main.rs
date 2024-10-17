90518e9f6c66fd18e4614d02fad7c79dc13b3c27
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-10-16 23:13:34 -0600

    feat: Add Rust service and proto files for rust service

--- Proyecto2/src/faculty_services/rust_service/src/main.rs
+++ Proyecto2/src/faculty_services/rust_service/src/main.rs
@@ -1,32 +1,55 @@
-use actix_web::{post, web, App, HttpServer, Responder, HttpResponse};
-use serde::Deserialize;
+use studentgrpc::student_client::StudentClient;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use studentgrpc::StudentRequest;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-struct Student {
-    student : String,
-    age: u32,
-    faculty: String,
-    discipline: u32,
+pub mod studentgrpc {
+    tonic::include_proto!("student");
 }
 
-#[post("/engineering")]
-async fn engineering_service(student: web::Json<Student>) ->  impl Responder {
-    println!(
-        "Recibido: Facultad = {}, Disciplina = {}, Estudiante = {}, Edad = {}",
-        student.faculty, student.discipline, student.student, student.age
-    );
-    
-    HttpResponse::Ok().body("Recibido")
+#[derive(Deserialize,Serialize,Debug)]
+struct StudentData {
+    student: String,
+    age: i32,
+    faculty: String,
+    discipline: i32,
+}
+
+async fn handle_student(student: web::Json<StudentData>) -> impl Responder {
+    println!("Received student data: {:?}", student);
+    let mut client = match StudentClient::connect("http://localhost:50051").await {
+        Ok(client) => client,
+        Err(e) => return HttpResponse::InternalServerError().body(format!("Failed to connect to gRPC server: {}", e)),
+    };
+
+    let request = tonic::Request::new(StudentRequest {
+        student: student.student.clone(),
+        age: student.age,
+        faculty: student.faculty.clone(),
+        discipline: student.discipline,
+    });
+
+    match client.get_student(request).await {
+        Ok(response) => {
+
+            println!("RESPONSE={:?}", response);
+
+            HttpResponse::Ok().json
+            (format!("Student: {:?}", response))
+        },
+        Err(e) => HttpResponse::InternalServerError().body(format!("gRPC call failed: {}", e)),
+    }
+
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    println!("Servicio de Rust corriendo en el puerto 8081");
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://localhost:8080");
     HttpServer::new(|| {
         App::new()
-            .service(engineering_service)
+            .route("/faculty", web::post().to(handle_student))
     })
-    .bind("0.0.0.0:8081")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
