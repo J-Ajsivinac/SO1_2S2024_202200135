5ebc0e8128358137df2e96ae8b6736989f1faef5
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-10-22 12:44:19 -0600

    refactor: Update Kafka consumer to use segmentio/kafka-go library

--- Proyecto2/src/consumers/winners/main.go
+++ Proyecto2/src/consumers/winners/main.go
@@ -1,41 +1,70 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/segmentio/kafka-go"
 )
 
-func main() {
-	kafkaBroker := "my-cluster-kafka-bootstrap:9092"
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": kafkaBroker,
-		"group.id":          "myGroup",
-		"auto.offset.reset": "earliest",
-	})
-
-	if err != nil {
-		log.Fatalf("Error creating consumer: %s", err)
-	}
-
-	defer consumer.Close()
-
-	topic := "winners"
-	err = consumer.SubscribeTopics([]string{topic}, nil)
-	if err != nil {
-		log.Fatalf("Error subscribing to topic: %s", err)
-	}
-
-	fmt.Printf("Esperando mensajes en el tópico %s...\n", topic)
-	for {
-		msg, err := consumer.ReadMessage(-1)
-		if err == nil {
-			fmt.Printf("Mensaje recibido: %s\n", string(msg.Value))
-		} else {
-			fmt.Printf("Error leyendo mensaje: %v (%v)\n", err, msg)
-		}
-	}
+type StudentOrder struct {
+	Student        string `json:"student"`         // Nombre del estudiante
+	Faculty        string `json:"faculty"`         // Facultad del estudiante
+	Age            int    `json:"age"`             // Edad del estudiante
+	Discipline     int    `json:"discipline"`      // Disciplina del estudiante
+	Winner         int    `json:"winner"`          // Indicador de si es un ganador (1 o 0)
+	DisciplineName string `json:"discipline_name"` // Nombre de la disciplina
 }
 
+func processEvent(event []byte) {
 
+	// unmarshal the data
+	var data StudentOrder
+	err := json.Unmarshal(event, &data)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal message: %s", err)
+		return
+	}
+
+	fmt.Println("Processing event: ", data)
+
+}
+
+func main() {
+	topic := "winners"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"my-cluster-kafka-bootstrap:9092"},
+		Topic:       topic,
+		Partition:   0,
+		MinBytes:    10e3, // 10KB
+		MaxBytes:    10e6, // 10MB
+		StartOffset: kafka.LastOffset,
+		GroupID:     "my-group",
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Println("failed to read message:", err)
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+
+		// Process the event
+		processEvent(m.Value)
+
+		// uncommit
+		err = r.CommitMessages(context.Background(), m)
+		if err != nil {
+			log.Println("failed to commit message:", err)
+		}
+
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
