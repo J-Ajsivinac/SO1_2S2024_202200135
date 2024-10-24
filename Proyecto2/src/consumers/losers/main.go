7b7f1530d6e091318a721c934e33242625dd7025
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-10-24 17:04:58 -0600

    refactor: Update image reference in athletics deployment

---
+++ Proyecto2/src/consumers/losers/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type StudentOrder struct {
+	Student        string `json:"student"`         // Nombre del estudiante
+	Faculty        string `json:"faculty"`         // Facultad del estudiante
+	Age            int    `json:"age"`             // Edad del estudiante
+	Discipline     int    `json:"discipline"`      // Disciplina del estudiante
+	Winner         int    `json:"winner"`          // Indicador de si es un ganador (1 o 0)
+	DisciplineName string `json:"discipline_name"` // Nombre de la disciplina
+}
+
+func processEvent(event []byte) {
+
+	// unmarshal the data
+	var data StudentOrder
+	err := json.Unmarshal(event, &data)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal message: %s", err)
+		return
+	}
+
+	fmt.Println("Processing event: ", data)
+
+}
+
+func main() {
+	topic := "losers"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"my-cluster-kafka-bootstrap:9092"},
+		Topic:       topic,
+		Partition:   0,
+		MinBytes:    10e3, // 10KB
+		MaxBytes:    10e6, // 10MB
+		StartOffset: kafka.LastOffset,
+		GroupID:     "my-group",
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Println("failed to read message:", err)
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+
+		// Process the event
+		processEvent(m.Value)
+
+		// uncommit
+		err = r.CommitMessages(context.Background(), m)
+		if err != nil {
+			log.Println("failed to commit message:", err)
+		}
+
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
